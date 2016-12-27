8462f10b0aed46d798243aadc7c300e7ce41cc6d
isomaru <naoki.since1978@gmail.com>
2016-12-27 13:56:25 +0900

    Initial Commit

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by 我妻 直樹 on 2016/12/27.
+//  Copyright © 2016年 Naoki Agatsuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
